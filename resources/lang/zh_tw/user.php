298f52cb1c792ab554f56b9c8309e75fc2815e73
mos <mos.tu@itec.com.tw>
2023-03-15 16:04:33 +0800

    feat: 新增語系檔

---
+++ resources/lang/zh_tw/user.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'name' => '使用者名稱',
+    'email' => '郵件地址',
+    'password' => '密碼',
+    'confirm_password' => '確認密碼',
+    'change_password' => '變更密碼',
+    'account_status' => '帳戶狀態',
+    'is_active' => '啟用',
+    'is_disabled' => '停用',
+    'created_at' => '建立時間',
+    'updated_at' => '更新時間',
+];
